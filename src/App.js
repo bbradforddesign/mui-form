090a2884ef548a3d9924d238237c431d750fff33
blakebford3 <blakebford3@gmail.com>
2019-08-21 21:43:23 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TextField, Paper, Typography, Button} from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const styles = ({ spacing: { unit } }) => ({
+  root: {
+    margin: unit,
+    padding: unit * 3,
+    maxWidth: 400
+  },
+  form: {
+    display: 'flex',
+    alignItems: 'baseline',
+    justifyContent: 'space-evenly'
+  },
+  TextField: {
+    marginBottom: unit * 2,
+  }
+})
 
-export default App;
+export default withStyles(styles)(
+  
+  class App extends React.Component {
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        name: "",
+        address: "",
+      }
+    }
+  
+    handleName = e => this.setState({
+      name: e.target.value
+    })
+  
+    handleAddress = e => this.setState({
+      address: e.target.value
+    })
+  
+    render() {
+  
+      const { classes } = this.props;
+
+      return (
+        <Paper className={classes.root}>
+          <Typography
+          variant="h6"
+          gutterBottom>
+            Check Eligibility
+          </Typography>
+            <form>
+              <TextField 
+              onChange={this.handleName}
+              placeholder="Name"
+              required/>
+              <TextField 
+              onChange={this.handleAddress}
+              placeholder="Address"
+              required/>
+            </form>
+            <Button
+            type="submit"
+            color="primary"
+            variant="raised">
+              Submit
+            </Button>
+        </Paper>
+      );
+    }
+  }
+)
+
